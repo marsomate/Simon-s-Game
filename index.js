16d84e3f62ae6f9715f57a027414818c6f6eaeff
Marso Mate <marsomate755@gmail.com>
2023-01-20 14:55:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ function nextSequence(){
     var randomChosenColour = buttonColours[randomNumber]
     gamePattern.push(randomChosenColour);
     $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-    var audio = new Audio("../sounds/" + randomChosenColour + ".mp3");
-  audio.play();
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
 }
-
 nextSequence();
