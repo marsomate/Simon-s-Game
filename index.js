21f7d7ed7b8e2cbc19b4b93a19e90afbc28a766d
Marso Mate <marsomate755@gmail.com>
2023-01-20 14:43:03 +0100

    q

--- index.js
+++ index.js
@@ -2,39 +2,14 @@ const gamePattern = [];
 
 const buttonColours = ["red", "blue", "green", "yellow"];
 
-var randomChosenColour = buttonColours[nextSequence()]
 
 function nextSequence(){
     var randomNumber = Math.floor(Math.random()*4);
-    return randomNumber;
+    var randomChosenColour = buttonColours[randomNumber]
+    gamePattern.push(randomChosenColour);
+    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    var audio = new Audio("../sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
 
-gamePattern.push(randomChosenColour);
-
-$("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-
-makeSound(randomChosenColour);
-
-function makeSound(randomChosenColour){
-    switch (randomChosenColour){
-        case "red":
-            var red = new Audio('sounds/red.mp3');
-            red.play();
-            break;
-        
-        case "blue":
-            var blue = new Audio('sounds/blue.mp3');
-            blue.play();
-        break;
-        
-        case "green":
-            var green = new Audio('sounds/green.mp3');
-            green.play();
-        break;
-        
-        case "yellow":
-            var yellow = new Audio('sounds/yellow.mp3');
-            yellow.play();
-        break;
-    }
-}
+nextSequence();
